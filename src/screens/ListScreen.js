458b9cd502db4c2fe48fa0e00f15b5d0b42c5069
Bashir <b_egeh@hotmail.com>
2020-06-19 22:00:33 -0400

    Initial commit - Udemy course react native chapters 1 to 6 (part way through)

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+const ListScreen = () => {
+  const friends = [
+    {name: "Friend #1", age: "11"},
+    {name: "Friend #2", age: "22"},
+    {name: "Friend #3", age: "33"},
+    {name: "Friend #4", age: "44"},
+    {name: "Friend #5", age: "45"},
+    {name: "Friend #6", age: "20"},
+    {name: "Friend #7", age: "29"},
+    {name: "Friend #8", age: "65"},
+    {name: "Friend #9", age: "56"},
+  ]
+  return (<FlatList 
+    keyExtractor={(friend) => friend.name }
+    data={friends}
+    // horizontal
+    // showsHorizontalScrollIndicator={false}
+    showsVerticalScrollIndicator={false}
+    renderItem={({ item }) => {
+    return <Text style={styles.textStyle}>
+      {item.name} - Age {item.age}
+      </Text>;
+    }}
+  />
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
+
+export default ListScreen;
